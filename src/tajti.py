2b40c89e1a63f14d6925010c799a4c2c81ed145f
CoGn151oN <121181789+CoGn151oN@users.noreply.github.com>
2023-05-17 17:40:49 +0200

    up

--- src/tajti.py
+++ src/tajti.py
@@ -11,7 +11,7 @@ def activation_tanh(x):     return numpy.tanh(x)  # (-1..1)
 def dactivation_tanh(x):    return 1.0 - x ** 2
 
 
-def activation_sigmoid(x):  return 1.0 / (1.0 + numpy.exp(-x))  # (0..1)
+def activation_sigmoid(x):  return 1.0 / (1.0 + numpy.exp(-x.astype(float)))  # (0..1)
 
 
 def dactivation_sigmoid(x): return x * (1.0 - x)
@@ -132,28 +132,35 @@ if __name__ == '__main__':
         [x_e, y_e] for x_e, y_e in zip(x_train, y_train)
     ])
 
-    # acti, dacti = activation_sigmoid, dactivation_sigmoid
-    acti, dacti = activation_tanh, dactivation_tanh
+    acti, dacti = activation_sigmoid, dactivation_sigmoid
+    # acti, dacti = activation_tanh, dactivation_tanh
 
     # samples = [[[0, 0], [0, 0]], [[0, 1], [0, 1]], [[1, 0], [0, 1]], [[1, 1], [1, 1]]]   # AND ĂŠs OR
-    # samples = [[[0, 0], [0, 0, 0]], [[0, 1], [0, 1, 1]], [[1, 0], [0, 1, 1]], [[1, 1], [1, 1, 0]]]  # AND, OR ĂŠs XOR
+    # samples_old = [[[0, 0], [0, 0, 0]], [[0, 1], [0, 1, 1]], [[1, 0], [0, 1, 1]], [[1, 1], [1, 1, 0]]]  # AND, OR ĂŠs XOR
+
+    # print(numpy.shape(samples_old[0][1]))
+    # print(numpy.shape(samples[0][1]))
+    # print(len(samples[0][0]))
 
     B = 1
-    # nn = [2+B, 4, IDE BAYY 3, 3]
+    # nn = [2+B, 4, ADD LAYERS 3, 3]
     nn = [len(samples[0][0]) + B, 4, 5, 3, len(samples[0][1])]
     wl = [numpy.random.random((nn[l + 1], nn[l])) * 0.8 - 0.4 for l in range(len(nn) - 1)]
+    # wl = [numpy.random.random((nn[l + 1], nn[l]-1)) * 0.8 - 0.4 for l in range(len(nn) - 1)]
     # wl = [numpy.linspace(-1,1,nn[l]*nn[l+1]).reshape((nn[l+1], nn[l])) for l in range(len(nn)-1)]
     delta = [numpy.zeros((nn[l + 1])) for l in range(len(nn) - 1)]
 
     epoch = 0
     sumerr = 1.0
-    while sumerr >= 0.01 and epoch <= 10000:
+    while sumerr >= 0.01 and epoch <= 50:
         sumerr = 0.0
         epoch += 1
         # for inp, out in random.sample(samples, len(samples)):
         for inp, out in samples:
-            nl = [numpy.array(inp + [1.0] * B)]
+            nl = [numpy.array(list(inp) + [1.0] * B)]
             for l in range(len(nn) - 1):
+                # print(wl[l].shape)
+                # print(nl[l].shape)
                 nl.append(acti(numpy.dot(wl[l], nl[l])))
             error = out - nl[-1]
             # delta = [None for _ in range(len(nn)-1)]
